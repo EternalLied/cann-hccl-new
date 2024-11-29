fbb5b51d6c632ac0af7cf930dfdc16344e4cc637
EternalLied <3165712904@qq.com>
2024-11-29 16:23:03 +0800

    Update coll_all_gather_double_ring_asym_executor.cc

--- src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_gather/coll_all_gather_double_ring_asym_executor.cc
+++ src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_gather/coll_all_gather_double_ring_asym_executor.cc
@@ -277,15 +277,31 @@ HcclResult CollAllGatherDoubleRingAsymExecutor::KernelRun(const OpParam &param, 
         CHK_RET(PrepareAllgatherSlice(level0RankSize, inputMemSize, dataSegsSlice));
 
         //  多环数据切分
-        // multRingsSliceZero = PrepareMultiRingSlice(dataSegsSlice, param.tag, false, topoAttr_.nicList);
-        // printf("multRingsSliceZero[0].size(): %d\n", multRingsSliceZero[0].size());
+        multRingsSliceZero = PrepareMultiRingSlice(dataSegsSlice, param.tag, false, topoAttr_.nicList);
+        printf("multRingsSliceZero.size(): %d\n", multRingsSliceZero.size());
+        printf("multRingsSliceZero[0].size(): %d\n", multRingsSliceZero[0].size());
+        for (size_t i = 0; i < multRingsSliceZero.size(); ++i) {
+            std::cout << "Ring " << i << ":\n";
+            for (size_t j = 0; j < multRingsSliceZero[i].size(); ++j) {
+                const Slice& slice = multRingsSliceZero[i][j];
+                std::cout << "  Slice " << j << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
+            }
+        }
 
-        std::vector<std::vector<Slice>> multRingsSliceZero;
+        std::vector<std::vector<Slice>> multRingsSameSliceZero;
         // 双环数据相同
         for (int i = 0; i < 2; ++i) {
-            multRingsSliceZero.push_back(dataSegsSlice);
+            multRingsSameSliceZero.push_back(dataSegsSlice);
         }    
-        // printf("multRingsSameSliceZero[0].size(): %d\n", multRingsSliceZero[0].size());
+        printf("multRingsSameSliceZero.size(): %d\n", multRingsSameSliceZero.size());
+        printf("multRingsSameSliceZero[0].size(): %d\n", multRingsSameSliceZero[0].size());
+        for (size_t i = 0; i < multRingsSameSliceZero.size(); ++i) {
+            std::cout << "Ring " << i << ":\n";
+            for (size_t j = 0; j < multRingsSameSliceZero[i].size(); ++j) {
+                const Slice& slice = multRingsSameSliceZero[i][j];
+                std::cout << "  Slice " << j << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
+            }
+        }
 
         std::vector<std::vector<Slice>> multRingsSlice;
 
