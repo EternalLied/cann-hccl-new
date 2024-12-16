63079c497bd92270eb9d86d3c0e7bf3b4ce44701
CLIMOiiin <93824537+CLIMOiiin@users.noreply.github.com>
2024-12-16 16:40:15 +0800

    Update alltoallv_pairwise_pro.cc

--- src/domain/collective_communication/algorithm/base/executor/alltoallv_pairwise_pro.cc
+++ src/domain/collective_communication/algorithm/base/executor/alltoallv_pairwise_pro.cc
@@ -233,41 +233,143 @@ HcclResult AlltoAllVPairWisePro::SendRecv(TxMemoryInfo txMemoryInfo, RxMemoryInf
 
 HcclResult AlltoAllVPairWisePro::RunZCopyAlltoAll(const u32 rank, const u32 rankSize, const std::vector<LINK> &links)
 {
-    for (u32 i = 1; i < rankSize; i++) {
-        u32 prevRank = (rank + rankSize - i) % rankSize;
-        u32 nextRank = (rank + i) % rankSize;
-        std::shared_ptr<Transport> prevTransport = links[prevRank];
-        std::shared_ptr<Transport> nextTransport = links[nextRank];
+    // 改进pairwise 总跳数为ranksize - 2
+    for (u32 i = 1; i < rankSize - 1; i++) {
 
-        CHK_SMART_PTR_NULL(prevTransport);
-        CHK_SMART_PTR_NULL(nextTransport);
+        if(i == 1){
+            // SIO对端
+            if (rank % 2 == 0){
+                u32 prevRank_s = rank + 1;
+                u32 nextRank_s = prevRank_s;
+            }
+            else{
+                u32 prevRank_s = rank - 1;  
+                u32 nextRank_s = prevRank_s;  
+            }
 
-        HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: prevRank[%u] nextRank[%u], step[%u]", prevRank, nextRank, i);
+            // HCCS对端
+            u32 prevRank_h = (rank + rankSize - i - 1) % rankSize;
+            u32 nextRank_h = (rank + i + 1) % rankSize;
 
-        CHK_RET(prevTransport->TxAck(stream_)); // transport sync record
-        CHK_RET(nextTransport->RxAck(stream_)); // transport sync wait
+            std::shared_ptr<Transport> prev_s_Transport = links[prevRank_s];
+            std::shared_ptr<Transport> next_s_Transport = links[nextRank_s];
 
-        u64 sendBytes = sendBuffer_.counts[nextRank] * sendDataUnitBytes_;
-        u64 recvBytes = recvBuffer_.counts[prevRank] * recvDataUnitBytes_;
-        u64 sendDispBytes = sendBuffer_.displs[nextRank] * sendDataUnitBytes_;
-        u64 recvDispBytes = recvBuffer_.displs[prevRank] * recvDataUnitBytes_;
-        u8 *sendAddr = reinterpret_cast<u8 *>(sendBuffer_.mem.ptr()) + sendDispBytes;
-        u8 *recvAddr = reinterpret_cast<u8 *>(recvBuffer_.mem.ptr()) + recvDispBytes;
+            std::shared_ptr<Transport> prev_h_Transport = links[prevRank_h];
+            std::shared_ptr<Transport> next_h_Transport = links[nextRank_h];
 
-        u64 dstOffset = rankRecvDisplsMap_.at(nextRank)[rank];
-        u64 srcOffset = rankSendDisplsMap_.at(prevRank)[rank];
+            CHK_SMART_PTR_NULL(prev_s_Transport);
+            CHK_SMART_PTR_NULL(next_s_Transport);
 
-        TxMemoryInfo txMemoryInfo{UserMemType::OUTPUT_MEM, dstOffset, sendAddr, sendBytes};
-        RxMemoryInfo rxMemoryInfo{UserMemType::INPUT_MEM, srcOffset, recvAddr, recvBytes};
+            CHK_SMART_PTR_NULL(prev_h_Transport);
+            CHK_SMART_PTR_NULL(next_h_Transport);
 
-        HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: sendBytes[%llu] recvBytes[%llu] sendDispBytes[%llu]" \
-            " dstOffset[%llu]", sendBytes, recvBytes, sendDispBytes, dstOffset);
-        HcclResult ret = SendRecv(txMemoryInfo, rxMemoryInfo, prevTransport, nextTransport);
-        CHK_PRT_RET(ret != HCCL_SUCCESS,
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: prevRank_s[%u] nextRank_s[%u], step[%u]", prevRank_s, nextRank_s, i);
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: prevRank_h[%u] nextRank_h[%u], step[%u]", prevRank_h, nextRank_h, i);
+
+            CHK_RET(prev_s_Transport->TxAck(stream_)); // transport sync record
+            CHK_RET(next_s_Transport->RxAck(stream_)); // transport sync wait
+
+            CHK_RET(prev_h_Transport->TxAck(stream_)); // transport sync record
+            CHK_RET(next_h_Transport->RxAck(stream_)); // transport sync wait
+
+            // SIO
+            u64 sendBytes_s = sendBuffer_.counts[nextRank_s] * sendDataUnitBytes_;
+            u64 recvBytes_s = recvBuffer_.counts[prevRank_s] * recvDataUnitBytes_;
+            u64 sendDispBytes_s = sendBuffer_.displs[nextRank_s] * sendDataUnitBytes_;
+            u64 recvDispBytes_s = recvBuffer_.displs[prevRank_s] * recvDataUnitBytes_;
+            u8 *sendAddr_s = reinterpret_cast<u8 *>(sendBuffer_.mem.ptr()) + sendDispBytes_s;
+            u8 *recvAddr_s = reinterpret_cast<u8 *>(recvBuffer_.mem.ptr()) + recvDispBytes_s;
+
+            u64 dstOffset_s = rankRecvDisplsMap_.at(nextRank_s)[rank];
+            u64 srcOffset_s = rankSendDisplsMap_.at(prevRank_s)[rank];
+
+            TxMemoryInfo txMemoryInfo_s{UserMemType::OUTPUT_MEM, dstOffset_s, sendAddr_s, sendBytes_s};
+            RxMemoryInfo rxMemoryInfo_s{UserMemType::INPUT_MEM, srcOffset_s, recvAddr_s, recvBytes_s};
+
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: sendBytes_s[%llu] recvBytes_s[%llu] sendDispBytes_s[%llu]" \
+            " dstOffset_s[%llu]", sendBytes_s, recvBytes_s, sendDispBytes_s, dstOffset_s);
+            HcclResult ret = SendRecv(txMemoryInfo_s, rxMemoryInfo_s, prevTransport, nextTransport);
+            CHK_PRT_RET(ret != HCCL_SUCCESS,
             HCCL_ERROR("[AlltoAllVPairWisePro][RunZCopyAlltoAll]errNo[0x%016llx] "\
-            "sendBytes[%llu] recvBytes[%llu] sendAddr[%p] dstOffset[%llu]",
-            HCCL_ERROR_CODE(ret), sendBytes, recvBytes, sendAddr, dstOffset),
+            "sendBytes_s[%llu] recvBytes_s[%llu] sendAddr_s[%p] dstOffset_s[%llu]",
+            HCCL_ERROR_CODE(ret), sendBytes_s, recvBytes_s, sendAddr_s, dstOffset_s),
             ret);
+
+            // HCCS
+            u64 sendBytes_h = sendBuffer_.counts[nextRank_h] * sendDataUnitBytes_;
+            u64 recvBytes_h = recvBuffer_.counts[prevRank_h] * recvDataUnitBytes_;
+            u64 sendDispBytes_h = sendBuffer_.displs[nextRank_h] * sendDataUnitBytes_;
+            u64 recvDispBytes_h = recvBuffer_.displs[prevRank_h] * recvDataUnitBytes_;
+            u8 *sendAddr_h = reinterpret_cast<u8 *>(sendBuffer_.mem.ptr()) + sendDispBytes_s;
+            u8 *recvAddr_h = reinterpret_cast<u8 *>(recvBuffer_.mem.ptr()) + recvDispBytes_s;
+
+            u64 dstOffset_h = rankRecvDisplsMap_.at(nextRank_h)[rank];
+            u64 srcOffset_h = rankSendDisplsMap_.at(prevRank_h)[rank];
+
+            TxMemoryInfo txMemoryInfo_h{UserMemType::OUTPUT_MEM, dstOffset_h, sendAddr_h, sendBytes_h};
+            RxMemoryInfo rxMemoryInfo_h{UserMemType::INPUT_MEM, srcOffset_h, recvAddr_h, recvBytes_h};
+
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: sendBytes_h[%llu] recvBytes_h[%llu] sendDispBytes_h[%llu]" \
+            " dstOffset_h[%llu]", sendBytes_h, recvBytes_h, sendDispBytes_h, dstOffset_h);
+            HcclResult ret = SendRecv(txMemoryInfo_h, rxMemoryInfo_h, prevTransport, nextTransport);
+            CHK_PRT_RET(ret != HCCL_SUCCESS,
+            HCCL_ERROR("[AlltoAllVPairWisePro][RunZCopyAlltoAll]errNo[0x%016llx] "\
+            "sendBytes_h[%llu] recvBytes_h[%llu] sendAddr_h[%p] dstOffset_h[%llu]",
+            HCCL_ERROR_CODE(ret), sendBytes_h, recvBytes_h, sendAddr_h, dstOffset_h),
+            ret);       
+
+        }
+        else{
+            //最后一跳对端选择 
+            if (i == rankSize - 2){
+                if (rank % 2 == 0){
+                    u32 prevRank = (rank - 1) % rankSize;
+                    u32 nextRank = prevRank;
+                }
+                else{
+                    u32 prevRank = (rank + 1) % rankSize;
+                    u32 nextRank = prevRank;
+                }
+            }
+            else{
+                u32 prevRank = (rank + rankSize - i - 1) % rankSize;
+                u32 nextRank = (rank + i + 1) % rankSize;
+            }
+
+            std::shared_ptr<Transport> prevTransport = links[prevRank];
+            std::shared_ptr<Transport> nextTransport = links[nextRank];
+
+            CHK_SMART_PTR_NULL(prevTransport);
+            CHK_SMART_PTR_NULL(nextTransport);
+
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: prevRank[%u] nextRank[%u], step[%u]", prevRank, nextRank, i);
+
+            CHK_RET(prevTransport->TxAck(stream_)); // transport sync record
+            CHK_RET(nextTransport->RxAck(stream_)); // transport sync wait
+
+            u64 sendBytes = sendBuffer_.counts[nextRank] * sendDataUnitBytes_;
+            u64 recvBytes = recvBuffer_.counts[prevRank] * recvDataUnitBytes_;
+            u64 sendDispBytes = sendBuffer_.displs[nextRank] * sendDataUnitBytes_;
+            u64 recvDispBytes = recvBuffer_.displs[prevRank] * recvDataUnitBytes_;
+            u8 *sendAddr = reinterpret_cast<u8 *>(sendBuffer_.mem.ptr()) + sendDispBytes;
+            u8 *recvAddr = reinterpret_cast<u8 *>(recvBuffer_.mem.ptr()) + recvDispBytes;
+
+            u64 dstOffset = rankRecvDisplsMap_.at(nextRank)[rank];
+            u64 srcOffset = rankSendDisplsMap_.at(prevRank)[rank];
+
+            TxMemoryInfo txMemoryInfo{UserMemType::OUTPUT_MEM, dstOffset, sendAddr, sendBytes};
+            RxMemoryInfo rxMemoryInfo{UserMemType::INPUT_MEM, srcOffset, recvAddr, recvBytes};
+
+            HCCL_DEBUG("[AlltoAllVPairWisePro][RunZCopyAlltoAll]: sendBytes[%llu] recvBytes[%llu] sendDispBytes[%llu]"
+                       " dstOffset[%llu]",
+                       sendBytes, recvBytes, sendDispBytes, dstOffset);
+            HcclResult ret = SendRecv(txMemoryInfo, rxMemoryInfo, prevTransport, nextTransport);
+            CHK_PRT_RET(ret != HCCL_SUCCESS,
+                        HCCL_ERROR("[AlltoAllVPairWisePro][RunZCopyAlltoAll]errNo[0x%016llx] "
+                                   "sendBytes[%llu] recvBytes[%llu] sendAddr[%p] dstOffset[%llu]",
+                                   HCCL_ERROR_CODE(ret), sendBytes, recvBytes, sendAddr, dstOffset),
+                        ret);
+        }
     }
 
     return HCCL_SUCCESS;
