03c23b21be0b986e41c871cc13a866360ed5fc60
EternalLied <3165712904@qq.com>
2024-12-25 19:21:30 +0800

    Revert "Update coll_aligned_reduce_scatter_asym_double_ring_executor.cc"
    This reverts commit de5b1688093708bbdeb198045159239a3f3e7699.

--- src/domain/collective_communication/algorithm/impl/coll_executor/coll_reduce_scatter/coll_aligned_reduce_scatter_asym_double_ring_executor.cc
+++ src/domain/collective_communication/algorithm/impl/coll_executor/coll_reduce_scatter/coll_aligned_reduce_scatter_asym_double_ring_executor.cc
@@ -205,21 +205,21 @@ HcclResult CollAlignedReduceScatterAsymDoubleRingExecutor::KernelRun(const OpPar
 
     // Print Slice
      
-    printf("dataSegsSlice.size(): %d\n", dataSegsSlice.size());
-    for (size_t i = 0; i < dataSegsSlice.size(); ++i) {
-            const Slice& slice = dataSegsSlice[i];
-            std::cout << "  Slice " << i << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
-    }
+    // printf("dataSegsSlice.size(): %d\n", dataSegsSlice.size());
+    // for (size_t i = 0; i < dataSegsSlice.size(); ++i) {
+    //         const Slice& slice = dataSegsSlice[i];
+    //         std::cout << "  Slice " << i << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
+    // }
 
-    printf("multiStreamSlice.size(): %d\n", multiStreamSlice.size());
-    printf("multiStreamSlice[0].size(): %d\n", multiStreamSlice[0].size());
-    for (size_t i = 0; i < multiStreamSlice.size(); ++i) {
-        std::cout << "Stream " << i << ":\n";
-        for (size_t j = 0; j < multiStreamSlice[i].size(); ++j) {
-            const Slice& slice = multiStreamSlice[i][j];
-            std::cout << "  Slice " << j << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
-        }
-    }
+    // printf("multiStreamSlice.size(): %d\n", multiStreamSlice.size());
+    // printf("multiStreamSlice[0].size(): %d\n", multiStreamSlice[0].size());
+    // for (size_t i = 0; i < multiStreamSlice.size(); ++i) {
+    //     std::cout << "Stream " << i << ":\n";
+    //     for (size_t j = 0; j < multiStreamSlice[i].size(); ++j) {
+    //         const Slice& slice = multiStreamSlice[i][j];
+    //         std::cout << "  Slice " << j << " - Offset: " << slice.offset << ", Size: " << slice.size << " bytes\n";
+    //     }
+    // }
 
     CalLevel0DataSegsSlice(execMem, multiStreamSlice, sliceNum, innerRankSize, level2RankSize, level0DataSegsSlice);
 
