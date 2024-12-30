ca0b579ac69ceffeb1893a61761a28a4b78beebc
EternalLied <3165712904@qq.com>
2024-12-30 14:29:11 +0800

    Update all_gather_operator.cc

--- src/domain/collective_communication/algorithm/impl/operator/all_gather_operator.cc
+++ src/domain/collective_communication/algorithm/impl/operator/all_gather_operator.cc
@@ -183,12 +183,13 @@ HcclResult AllGatherOperator::SelectAlgfor91093(const OpParam& param, std::strin
             algName = "AllGatherComm";
         }
     }
+    algName = "AlignedAllGatherAsymDoubleRingExecutor";
     HCCL_INFO("[SelectAlgfor91093] all_gather SelectAlgfor91093 is algName [%s]", algName.c_str());
     // algName = "AllGatherDoubleRingAsymExecutor";
     // algName = "AllGatherDoubleRingConcurrentExecutor";
     // algName = "AlignedAllGatherDoubleRingFor91093Executor";
     // algName = "AlignedAllGatherAsymDoubleRingExecutor";
-    HCCL_INFO("[SelectAlgfor91093] all_gather SelectAlgfor91093 is algName [%s] finally", algName.c_str());
+    // HCCL_INFO("[SelectAlgfor91093] all_gather SelectAlgfor91093 is algName [%s] finally", algName.c_str());
     return HCCL_SUCCESS;
 }
 
