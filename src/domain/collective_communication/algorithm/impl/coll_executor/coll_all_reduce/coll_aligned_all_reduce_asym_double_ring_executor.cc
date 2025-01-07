2f915dfa15327109062fff900244cf175691e4eb
CLIMOiiin <93824537+CLIMOiiin@users.noreply.github.com>
2025-01-07 11:10:37 +0800

    all reduce

--- src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_reduce/coll_aligned_all_reduce_asym_double_ring_executor.cc
+++ src/domain/collective_communication/algorithm/impl/coll_executor/coll_all_reduce/coll_aligned_all_reduce_asym_double_ring_executor.cc
@@ -30,49 +30,56 @@ HcclResult CollAlignedAllReduceAsymDoubleRingExecutor::DoubleRingReduceScatter(c
     const std::vector<std::vector<Slice>> multRingsUserMemSlice, const bool retryEnable)
 {
     (void)tag;
-    HCCL_INFO("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] DoubleRingReduceScatter starts");
+    HCCL_INFO(
+        "[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] DoubleRingReduceScatter starts");
     HcclResult ret = HCCL_SUCCESS;
     u32 ringNum = multRingsSliceZero.size();
     CHK_RET(CheckCommSize(COMM_LEVEL0, ringNum));
+
     // 拿到ring环映射关系
     SubCommInfo outerZeroCommInfo = GetSubCommInfo(COMM_LEVEL0, COMM_INDEX_0);
     auto nicList = topoAttr_.nicList;
     std::vector<std::vector<u32>> multiRingsOrder =
         GetRingsOrderByTopoType(outerZeroCommInfo.localRankSize, topoType_, nicList);
+
     u64 reduceAttr = GetReduceAttr(inputMem, outputMem, dataType, reductionOp);
+
     SubCommInfo outerRingCommInfo = GetSubCommInfo(COMM_LEVEL0, COMM_INDEX_0);
     // 生成两个ring上的userMemIn_上对应的slices
     std::vector<std::vector<Slice>> userMemInputSlicesOfDoubleRing;
-    CHK_RET(CollectMultiRingsUserMemSlices(ringNum, dataType, opInfo, multRingsSliceZero,
-        multiRingsOrder, multRingsUserMemSlice, userMemInputSlicesOfDoubleRing));
+    CHK_RET(CollectMultiRingsUserMemSlices(ringNum, dataType,
+        opInfo, multRingsSliceZero,
+        multiRingsOrder, multRingsUserMemSlice,
+        userMemInputSlicesOfDoubleRing));
     // 生成两个ring上的rankOrder
     std::vector<std::vector<u32>> rankOrders;
     CHK_RET(CollectMultiRingsRankOrder(ringNum, multiRingsOrder, rankOrders));
     // 初始化executor
     std::unique_ptr<ExecutorBase> executor;
-    executor.reset(new (std::nothrow) AlignedReduceScatterAsymDoubleRing(dispatcher_,
-        reduceAttr, opInfo, topoAttr_.userRank, algResResp_->slaveStreams, algResResp_->notifiesM2S,
-        algResResp_->notifiesS2M, rankOrders, userMemInputSlicesOfDoubleRing));
+    executor.reset(new (std::nothrow) AlignedReduceScatterAsymDoubleRing(
+        dispatcher_, reduceAttr, opInfo, topoAttr_.userRank, algResResp_->slaveStreams,
+        algResResp_->notifiesM2S, algResResp_->notifiesS2M, rankOrders, userMemInputSlicesOfDoubleRing));
     CHK_SMART_PTR_NULL(executor);
-    ret = executor->Prepare(inputMem, inputMem, outputMem, count, dataType, stream,
-        multRingsSliceZero, reductionOp, OUTER_BRIDGE_RANK_ID, baseOffset, retryEnable);
+    ret = executor->Prepare(inputMem, inputMem, outputMem, count, dataType, stream, multRingsSliceZero,
+        reductionOp, OUTER_BRIDGE_RANK_ID, baseOffset, retryEnable);
     CHK_PRT_RET(ret != HCCL_SUCCESS,
-        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring "
-                   "reduce scatter failed failed,return[%d]", ret), ret);
+        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring reduce scatter failed"
+        "failed,return[%d]", ret), ret);
     u32 ringIndexOp = COMM_INDEX_0;
     u32 rankSize = outerRingCommInfo.localRankSize;
-    ret = executor->RegisterProfiler(((ringIndexOp + 1) << PROF_RINGINDEX_OFFSET_OF_PLANEID) +
-        (rankSize << PROF_RANKSIZE_OFFSET_OF_PLANEID) + outerRingCommInfo.localRank, profStage,
-        HCCL_EXEC_STEP_NOT_SET, stream);
+    ret = executor->RegisterProfiler(
+        ((ringIndexOp + 1) << PROF_RINGINDEX_OFFSET_OF_PLANEID) +
+        (rankSize << PROF_RANKSIZE_OFFSET_OF_PLANEID) + outerRingCommInfo.localRank,
+        profStage, HCCL_EXEC_STEP_NOT_SET, stream);
     CHK_PRT_RET(ret != HCCL_SUCCESS,
-        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring "
-                   "reduce scatter failed failed,return[%d]", ret), ret);
+        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring reduce scatter failed "
+        "failed,return[%d]", ret), ret);
 
     CHK_RET(ExecutorBase::ExecEmptyTask(inputMem, outputMem, stream, dispatcher_));
     ret = RunTemplate(executor, outerRingCommInfo);
     CHK_PRT_RET(ret != HCCL_SUCCESS,
-        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring "
-                   "reduce scatter failed failed,return[%d]", ret), ret);
+        HCCL_ERROR("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingReduceScatter] Double ring reduce scatter failed "
+        "failed,return[%d]", ret), ret);
 
     CHK_RET(ExecutorBase::ExecEmptyTask(inputMem, outputMem, stream, dispatcher_));
     return HCCL_SUCCESS;
@@ -88,10 +95,17 @@ HcclResult CollAlignedAllReduceAsymDoubleRingExecutor::DoubleRingAllGather(
     HCCL_INFO("[CollAlignedAllReduceAsymDoubleRingExecutor][DoubleRingAllGather] DoubleRingAllGather starts");
     HcclResult ret = HCCL_SUCCESS;
     u32 ringNum = multRingsSliceZero.size();
-    CHK_RET(CheckCommSize(COMM_LEVEL0, ringNum));
-    // 拿到ring环映射关系
-    SubCommInfo outerZeroCommInfo = GetSubCommInfo(COMM_LEVEL0, COMM_INDEX_0);
-    auto nicList = topoAttr_.nicList;
+    // CHK_RET(CheckCommSize(COMM_LEVEL0, ringNum));
+    // // 拿到ring环映射关系
+    // SubCommInfo outerZeroCommInfo = GetSubCommInfo(COMM_LEVEL0, COMM_INDEX_0);
+    // 获取打平通信域
+    CHK_RET(CheckCommSize(COMM_COMBINE_ORDER, COMM_INDEX_0 + 1));
+    SubCommInfo outerZeroCommInfo = GetSubCommInfo(COMM_COMBINE_ORDER, COMM_INDEX_0);
+    // auto nicList = topoAttr_.nicList;
+    std::vector<u32> nicList;
+    for (int i = 0; i < 32; i++) {
+        nicList.push_back(i);
+    }
     std::vector<std::vector<u32>> multiRingsOrder =
         GetRingsOrderByTopoType(outerZeroCommInfo.localRankSize, topoType_, nicList);
     // 生成两个ring上的userMemOut_上对应的slices
